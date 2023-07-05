3929d3f6d9bb35fa51dbc08c8069be9af72b34a0
Makda Dejene <makdadejene@gmail.com>
2023-07-05 21:06:43 +0000

    Exercise 4

--- src/blur.ml
+++ src/blur.ml
@@ -1,8 +1,24 @@
 open Core
 
-(* You need to modify this function to blur the input image
-   based on the provided radius instead of ignoring it. *)
-let transform image ~radius:_ = image
+(* You need to modify this function to blur the input image based on the
+   provided radius instead of ignoring it. *)
+let transform image ~radius =
+  Image.mapi image ~f:(fun ~x ~y _pix ->
+    let x_start = if x - radius < 0 then 0 else x - radius in
+    let x_end =
+      if x + radius > Image.width image
+      then Image.width image
+      else x + radius
+    in
+    let y_start = if y - radius < 0 then 0 else y - radius in
+    let y_end =
+      if y + radius > Image.height image
+      then Image.height image
+      else y + radius
+    in
+    let rad = Image.slice image ~x_start ~x_end ~y_start ~y_end in
+    Image.mean_pixel rad)
+;;
 
 let command =
   Command.basic
@@ -24,5 +40,6 @@ let command =
         let image' = transform image ~radius in
         Image.save_ppm
           image'
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
 ;;
--- src/dither.ml
+++ src/dither.ml
@@ -1,7 +1,38 @@
 open Core
 
 (* This should look familiar by now! *)
-let transform image = image
+let transform image = 
+
+
+Image.foldi image ~init:image ~f:(fun ~x ~y (r,g,b) ->
+  let max = Image.max_val image in
+ let val = (Int.of_float r)/.(Int.of_float max) in
+ if(val > 0.5 ) then 
+ (let error = Int.of_float val-1.0)
+ val = 1.0 
+else 
+  (let error = Int.of_float val-0.5)
+ val = 0.5
+
+ let seven = Int.of_float error*(7/16) in
+ let three = Int.of_float error*(3/16) in
+ let five = Int.of_float error*(5/16) in
+ let one = Int.of_float error*(1/16) in
+
+  let right = Image.set ~x: x + 1 ~y:y image (seven, seven, seven) 
+  let diag_left = Image.set ~x: x -1 ~y:y-1 image (three, three, three)
+  let bottom = Image.set ~x: x ~y;y-1 image (five, five, five)
+  let diag_right = Image.set ~x:x+1 ~y:y-1 image (one, one, one)
+
+    
+
+)
+;;
+
+
+
+
+
 
 let command =
   Command.basic
